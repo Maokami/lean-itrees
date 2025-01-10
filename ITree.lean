59c895f4f70cd84ad6c8d6524a605eaaab26fe87
Max Nowak <max.nowak.j@gmail.com>
2025-01-10 15:07:34 -0800

    License

--- ITree.lean
+++ ITree.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author(s): Max Nowak
+-/
+
 import ITree.ITree
 import ITree.Eutt
 import ITree.Monad
--- ITree/Notation/Cat.lean
+++ ITree/Notation/Cat.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author(s): Max Nowak
+-/
+
 /-- Concatenation of continuations. Provides the `>>>` notation. -/
 class Cat (K : Type u -> Type u -> Type u) where
   cat : K A B -> K B C -> K A C
